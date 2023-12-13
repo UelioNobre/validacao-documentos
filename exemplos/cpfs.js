3f60304d4130fbaec8bf54c8e800944bb3d381e6
uelionobre <uelionobre@hotmail.com>
2023-12-13 13:34:53 -0300

    Remove arquivos index e de exemplos de cpf

---
+++ exemplos/cpfs.js
@@ -0,0 +1,14 @@
+const cpfsValidos = [
+  "529.982.247-25",
+  "84944390068",
+];
+
+const cpfsInvalidos = [
+  "040.738.473-18",
+  "989.494.986-49",
+  "989.49x.986-49",
+  "989.49x.986-4-",
+  "29.982.247-255",
+];
+
+module.exports = { cpfsValidos, cpfsInvalidos };
